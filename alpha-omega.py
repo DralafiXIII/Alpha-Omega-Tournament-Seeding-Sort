47f13e10fd1a292aa7ec1e9e2df739dec269eaab
Sean Settgast <dralafi13@gmail.com>
2020-10-10 11:05:51 -0400

    Finalized and added comments for clarification

--- alpha-omega.py
+++ alpha-omega.py
@@ -3,15 +3,18 @@
 
 import math
 
+# Creates a list of teams
 seeds = []
-for i in range(64):
+for i in range(33):
     seeds.append([i + 1,""])
 
 def keyGet(seeds):
     return seeds[0]
 
+# Sorts list of teams. Necessary when the list is generated manually.
 seedsSorted = sorted(seeds, key=keyGet)
 
+# Generates byes
 twoN = 0
 for i in range(1, len(seedsSorted)):
     temp = 2**i
@@ -22,18 +25,21 @@ for i in range(1, len(seedsSorted)):
 for i in range(twoN - len(seedsSorted)):
     seedsSorted.append([seedsSorted[len(seedsSorted) - 1][0] + 1, "BYE"])
 
+# Prints list before alpha-omega sorting. Demonstrates that the list has been properly sorted and that byes have been properly generated.
 print(seedsSorted)
 
-for i in range(math.ceil(math.log(len(seedsSorted)))): # <-- this is where the problem is
+# Alpha-omega sorting algorithm
+for i in range(math.ceil(math.log(len(seedsSorted)))):
     temp = []
     for j in range(0,int(len(seedsSorted) / 2),2**i):
         for k in range(2**i):
             temp.append(seedsSorted[j + k])
-            print(temp)
         for k in range(2**i,0,-1):
             temp.append(seedsSorted[len(seedsSorted) - (j + k)])
-            print(temp)
     seedsSorted = temp
+
+# Prints list after alpha-omega sorting. Data is now ready to be brought to the front-end for use.
+print(seedsSorted)
 
 """
 
